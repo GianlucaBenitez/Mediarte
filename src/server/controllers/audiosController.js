b22e4788dbf746a6d209724322db1aa51bb92d3f
toaruaj <tobiasaraujo@gmail.com>
2024-09-04 16:16:23 -0300

    terminar controllers

--- src/server/controllers/audiosController.js
+++ src/server/controllers/audiosController.js
@@ -49,6 +49,43 @@ const audiosController = {
     }catch (error) {
       return res.status(500).json({error: "Internal Server Error"})
     }
+  },
+  actualizar: async (req, res) => {
+    try {
+      const { guardados } = req.body;
+      const id = req.params.id;
+      if (!usuario) {
+        return res.status(404).json({ error: "Audio no encontrado" });
+      }
+  
+      const audioEnUso = await Audio.findOne({ where: { Audio: Audio } });
+      if (audioEnUso && audioEnUso.id_audio != id) {
+        return res.status(409).json({ error: "Ese audio ya esta guardado" });
+      }
+  
+      await usuario.save();
+  
+      return res.status(200).json({ message: "Audio  actualizado exitosamente", data: Audio});
+    } catch (error) {
+      return res.status(500).json({error: "Internal Server Error"})
+    }
+  },
+  
+  borrar: async (req, res) => {
+    try {
+      const id = req.params.id;
+  
+      const nombreAudio = await Audio.findByPk(id);
+      if (!Audio) {
+        return res.status(404).json({ error: "Usuario no encontrado" });
+      }
+  
+      await Audio.destroy({ where: { id_audio: id } });
+  
+      return res.status(200).json({ message: "Audio borrado", data: usuario});
+    } catch (error) {
+      return res.status(500).json({error: "Internal Server Error"})
+    }
   }
 }
 
