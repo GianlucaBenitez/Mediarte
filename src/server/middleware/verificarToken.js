127fd45c072057524fa6ff22ee4ab24dcca5338e
GianlucaBenitez <benitezgianluca9@gmail.com>
2024-10-29 10:38:14 -0300

    Authorization header

--- src/server/middleware/verificarToken.js
+++ src/server/middleware/verificarToken.js
@@ -3,7 +3,7 @@ import {config} from "dotenv";
 config();
 
 const verificarToken = (req, res, next) => {
-  const token = req.cookies.token || req.headers.authorization.split(" ")[1];
+  const token = req.cookies.token || req.headers.authorization;
 
   if (!token) {
     return res.status(401).json({ error: "Acceso denegado. Token no proporcionado." });
