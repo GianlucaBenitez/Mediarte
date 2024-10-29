bc8240a3d853e635211ee6f7ebf283644e7fc823
GianlucaBenitez <benitezgianluca9@gmail.com>
2024-10-29 11:04:18 -0300

    Agregando split al token

--- src/server/middleware/verificarToken.js
+++ src/server/middleware/verificarToken.js
@@ -3,7 +3,7 @@ import {config} from "dotenv";
 config();
 
 const verificarToken = (req, res, next) => {
-  const token = req.cookies.token || req.headers.authorization;
+  const token = req.cookies.token || req.headers.authorization.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ error: "Acceso denegado. Token no proporcionado." });
